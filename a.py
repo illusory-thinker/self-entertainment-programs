4b5d35b498364d0b6639a75acf4269bbd56f0dd7
illusory-thinker <fsbtsyc@163.com>
2019-10-19 16:59:54 +0800

    nothing

---
+++ a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 16:53:25 2019
+
+@author: Quantum Mechanics
+
+Perseverance always pays off
+"""
+
+lst1=['肥宅','学习怪','渣男','傻逼','菜鸡','大佬']
+import random
+random.shuffle(lst1)
+print(lst1)
